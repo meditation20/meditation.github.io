e80684300ff22caf1f411c1b83a927988a4cf99c
meditation <meditation692@gmail.com>
2024-10-18 15:35:42 +0530

    added files

---
+++ main.js
@@ -0,0 +1,38 @@
+new Morris.Line({
+    // ID of the element in which to draw the chart.
+    element: 'myfirstchart',
+    lineColors:['orange'],
+    // Chart data records -- each entry in this array corresponds to a point on
+    // the chart.
+    data: [
+      { year: '2008', value: 20 },
+      { year: '2009', value: 10 },
+      { year: '2010', value: 25 },
+      { year: '2011', value: 5 },
+      { year: '2012', value: 30 },
+      { year: '2012', value: 20 },
+      { year: '2012', value: 40 },
+      { year: '2012', value: 50 },
+    ],
+    // The name of the data record attribute that contains x-values.
+    xkey: 'year',
+    // A list of names of data record attributes that contain y-values.
+    ykeys: ['value'],
+    // Labels for the ykeys -- will be displayed when you hover over the
+    // chart.
+    labels: ['Value']
+  });
+
+  Morris.Donut({
+    element:'donut-example',
+    data:[
+        {label:"download sales", value:12},
+        {label:"In-Store sales", value:30},
+        {label:"Mail-order sales", value:40},
+    ],
+    colors:['orange','blue','white'],
+    labelColor:'#ffffff'
+  })
+
+
+
